e68079dfea7946853cdcd298028d76024b7e8628
João Lucas <62743404+joaoluke@users.noreply.github.com>
2023-02-08 08:11:49 -0300

    hello world in quantum computing

---
+++ main.py
@@ -0,0 +1,15 @@
+from qiskit import QuantumCircuit, execute, Aer
+
+qc = QuantumCircuit(2, 2)
+
+qc.h(0)
+qc.cx(0, 1)
+
+qc.measure([0, 1], [0, 1])
+
+backend = Aer.get_backend("qasm_simulator")
+job = execute(qc, backend, shots=1024)
+result = job.result()
+counts = result.get_counts(qc)
+
+print("Resultados:", counts)
