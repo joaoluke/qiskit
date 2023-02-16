5bad722a1c0d75e9d81381fd6c225cdb770285d0
João Lucas <62743404+joaoluke@users.noreply.github.com>
2023-02-15 22:46:36 -0300

    Add an Rx port on one of the qubits before the CX ports

--- circuit_basic.py
+++ circuit_basic.py
@@ -6,11 +6,16 @@ from qiskit.visualization import array_to_latex
 # Create a Quantum Circuit acting on a quantum register of three qubits
 circ = QuantumCircuit(3)
 
+# Add an Rx gate on qubit 0 to rotate the state before the CX gates
+circ.rx(np.pi/2, 0)
+
 # Add a H gate on qubit 0, putting this qubit in superposition.
 circ.h(0)
+
 # Add a CX (CNOT) gate on control qubit 0 and target qubit 1, putting
 # the qubits in a Bell state.
 circ.cx(0, 1)
+
 # Add a CX (CNOT) gate on control qubit 0 and target qubit 2, putting
 # the qubits in a GHZ state.
 circ.cx(0, 2)
@@ -22,6 +27,4 @@ state = Statevector.from_int(0, 2**3)
 state = state.evolve(circ)
 
 #draw using latex
-
-#Alternative way of representing in latex
 array_to_latex(state)
